660cf52802071b9e1f7bc4772d529cb0528cc6d0
ivanvelykyi <i-velikiy@mail.ua>
2019-10-26 16:55:43 +0300

    26.10 Add Locality Seeder

---
+++ database/migrations/2019_10_26_132727_create_localities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocalitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('localities', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('code',10);
+            $table->string('name',128);
+            $table->string('parent_code',10);
+            $table->boolean('center');
+            $table->char('type',1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('localities');
+    }
+}
