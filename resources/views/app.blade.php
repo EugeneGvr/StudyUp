c60f136c524589e0acc84b6596f7605cc8b411f2
Evhenii <link6596@gmail.com>
2020-07-09 15:48:15 +0300

    server changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <meta name="csrf-token" content="{!! csrf_token() !!}"/>
-    <script src="{{ mix('/js/app.js') }}" defer></script>
+    <script src="{{ asset('public/js/app.js') }}" defer></script>
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons">
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
     @routes
 </head>
 <body class="font-sans leading-none text-grey-darkest antialiased">
